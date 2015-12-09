6613c0f8ca82a5136d5de484af0fb7200f21c616
Joel Barker <joel@mojamail.com>
2015-12-08 18:30:03 -0800

    gut index.php and create a Demo class

---
+++ lib/Protobellum/Demo.php
@@ -0,0 +1,66 @@
+<?php
+namespace Protobellum;
+
+class Demo
+{
+    protected $armies = [];
+
+    public function __construct()
+    {
+        $this->armies[] = new Army('Ravens of Doom',    rand(1000, 10000));
+        $this->armies[] = new Army('Lumbering Sloths',  rand(1000, 10000));
+
+        return;
+    }
+
+    public function wageWar()
+    {
+        if (! $this->runSanityChecks()) {
+            return;
+        }
+
+        while (count($this->armies) > 1) {
+            $attacker   = $this->getNextArmy();
+            $victim     = $this->getNextArmy($attacker);
+            
+            echo "<i>The $attacker->name have attacked the $victim->name!</i><br/>";
+            $attacker->attack($victim);
+
+            $attacker->muster();
+            $victim->muster();
+
+            if ($attacker->surrendered || $victim->surrendered) {
+                $this->armies = array_filter($this->armies, function ($army) { ! $army->surrendered; });
+            }
+        }
+
+        return;
+    }
+
+    protected function runSanityChecks()
+    {
+        // make sure there are no duplicate armies
+        $this->armies = array_unique($this->armies, SORT_REGULAR);
+
+        // make sure there are multiple armies left
+        if (count($this->armies) < 2) {
+            trigger_error('There are not enough armies to wage war!', E_USER_ERROR);
+            return false;
+        }
+
+        // all checks passed!
+        return true;
+    }
+
+    protected function getNextArmy(Army $excluded = null)
+    {
+        $candidate = null;
+
+        do {
+            $numberOfArmies = count($this->armies);
+            $candidate      = $this->armies[ rand(0, $numberOfArmies - 1) ];
+        } while ($candidate == $excluded);
+        
+        return $candidate;
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,26 +1,7 @@
 <?php
 require('../vendor/autoload.php');
 
+use Protobellum\Demo;
 use Protobellum\Army;
 
-$me     = new Army('Ravens of Mayhem', rand(1000, 10000));
-$foe    = new Army('Lumbering Sloths', rand(1000, 10000));
-
-$me->muster();
-$foe->muster();
-
-while (! $me->surrendered && ! $foe->surrendered) {
-    echo '<hr/>';
-
-    if (rand(0, 1) == 0) {
-        echo "<i>The $me->name have attacked!</i><br/>";
-        $me->attack($foe);
-    } else {
-        echo "<i>The $foe->name have attacked!</i><br/>";
-        $foe->attack($me);
-    }
-
-    $me->muster();
-    $foe->muster();
-}
-
+(new Demo())->wageWar();
